9685c0558450d9576ade0388fe1d81d24cb59bad
Farazulhaque <farazattari786@gmail.com>
2022-02-11 19:50:09 +0530

    formatted

--- myjScript.js
+++ myjScript.js
@@ -1,17 +1,13 @@
-$(document).ready(function(){
-            
-    $(window).resize(function () {
-        if ($(window).width() < 700) {
-            $(".nav-bgcolor").removeClass('nav-bgcolor')
-        }
-        else{
-          $(".navbar").addClass('nav-bgcolor')
-        } 
-  })
-  
-  // $( document ).click(function() {
-  //   $( "#mobilenav" ).toggle( "slide" );
-  // });
+$(document).ready(function () {
+	$(window).resize(function () {
+		if ($(window).width() < 700) {
+			$(".nav-bgcolor").removeClass("nav-bgcolor");
+		} else {
+			$(".navbar").addClass("nav-bgcolor");
+		}
+	});
 
-  
+	// $( document ).click(function() {
+	//   $( "#mobilenav" ).toggle( "slide" );
+	// });
 });
