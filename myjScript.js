56db47483f2caaa69b200c4205f1c9f90c852c0a
Waliur <waliurrahman42@gmail.com>
2022-02-14 03:07:04 +0530

    some change
    so

--- myjScript.js
+++ myjScript.js
@@ -1,17 +1,51 @@
 $(document).ready(function(){
             
-    $(window).resize(function () {
-        if ($(window).width() < 700) {
-            $(".nav-bgcolor").removeClass('nav-bgcolor')
-        }
-        else{
-          $(".navbar").addClass('nav-bgcolor')
-        } 
-  })
+  //   $(window).resize(function () {
+  //       if ($(window).width() < 700) {
+  //           $(".nav-bgcolor").removeClass('nav-bgcolor')
+  //       }
+  //       else{
+  //         $(".navbar").addClass('nav-bgcolor')
+  //       } 
+  // })
   
   // $( document ).click(function() {
   //   $( "#mobilenav" ).toggle( "slide" );
   // });
 
+
+  var fixHeight = function() {
+    $('.navbar-nav').css(
+      'max-height',
+      document.documentElement.clientHeight - 150
+    );
+  };
+  fixHeight();
   
+  $(window).resize(function() {
+    fixHeight();
+  });
+
+  $('.navbar .navbar-toggler').on('click', function() {
+    fixHeight();
+  });
+  
+
+  $('.navbar-toggler, .overlay').on('click', function() {
+    $('.mobileMenu, .overlay').toggleClass('open');
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
