ec4bb1c7be07794a636eb1f464e038a44012d983
Adeel <mdadeel140891@gmail.com>
2022-02-25 21:38:53 +0530

    changes by adeel

--- Adeel/myjScript.js
+++ Adeel/myjScript.js
@@ -1,17 +1,62 @@
 $(document).ready(function(){
             
-    $(window).resize(function () {
-        if ($(window).width() < 700) {
-            $(".nav-bgcolor").removeClass('nav-bgcolor')
-        }
-        else{
-          $(".navbar").addClass('nav-bgcolor')
-        } 
-  })
+  //   $(window).resize(function () {
+  //       if ($(window).width() < 700) {
+  //           $(".nav-bgcolor").removeClass('nav-bgcolor')
+  //       }
+  //       else{
+  //         $(".navbar").addClass('nav-bgcolor')
+  //       } 
+  // })
   
-  // $( document ).click(function() {
-  //   $( "#mobilenav" ).toggle( "slide" );
-  // });
+  
 
+
+  var fixHeight = function() {
+    $('.navbar-nav').css(
+      'max-height',
+      document.documentElement.clientHeight - 150
+    );
+  };
+  fixHeight();
   
+  $(window).resize(function() {
+    fixHeight();
+  });
+
+  $('.navbar .navbar-toggler').on('click', function() {
+    fixHeight();
+  });
+  
+
+  $('.navbar-toggler,.overlay,.btnclsnav').on('click', function() {
+    $('.mobileMenu,.overlay,.btnclsnav').toggleClass('open');
+  });
+
+
+
+
+  $('.carousel').carousel({
+    interval: 3000,
+    keyboard: true,
+    pause: 'hover',
+    wrap: true
+  });
+
+  $('#slider4').on('slide.bs.carousel', function () {
+    console.log('SLIDE!');
+  });
+
+  $('#slider4').on('slid.bs.carousel', function () {
+    console.log('SLID!');
+  });
+
+
+
+
+
+
+
+
+
 });
