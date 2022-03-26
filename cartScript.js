52632c456cf85ed9a77d1f8d91389eb7abfa9b01
Farazulhaque <farazattari786@gmail.com>
2022-03-26 20:06:43 +0530

    added mobile responsiveness

--- cartScript.js
+++ cartScript.js
@@ -9,13 +9,22 @@ function updateItemPrice(id) {
 
 function updateTotal() {
     var total = 0;
+    var discount = 0;
     var i = 1;
     while (document.getElementById("item" + i + "total") != null) {
         total += parseFloat(document.getElementById("item" + i + "total").innerHTML)
         i++;
     }
     document.getElementById("total").innerHTML = "&#x20b9; " + total;
-    var discount = total * 0.05
-    document.getElementById("discount").innerHTML = "&#x20b9; " + discount;
+
+    if (total < 500) {
+        document.getElementById("delivery-fee").innerHTML = "&#x20b9; " + (total * 0.05);
+        document.getElementById("discount").innerHTML = "&#x20b9; " + 0;
+    }
+    if (total > 500) {
+        discount = total * 0.05
+        document.getElementById("discount").innerHTML = "&#x20b9; " + discount;
+        document.getElementById("delivery-fee").innerHTML = "&#x20b9; " + 0;
+    }
     document.getElementById("grandTotal").innerHTML = "&#x20b9; " + (total - discount);
 }
