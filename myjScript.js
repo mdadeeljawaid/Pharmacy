68ef6b8a8a4e14833ebf705114dba206fe8fb251
Waliur <waliurrahman42@gmail.com>
2022-04-01 16:39:31 +0530

    myjsupdate

---
+++ myjScript.js
@@ -0,0 +1,63 @@
+$(document).ready(function(){
+
+    //   $(window).resize(function () {
+    //       if ($(window).width() < 700) {
+    //           $(".nav-bgcolor").removeClass('nav-bgcolor')
+    //       }
+    //       else{
+    //         $(".navbar").addClass('nav-bgcolor')
+    //       } 
+    // })
+  
+  
+  //for nav on mobile view
+  
+    var fixHeight = function() {
+      $('.navbar-nav').css(
+        'max-height',
+        document.documentElement.clientHeight - 150
+      );
+    };
+    fixHeight();
+  
+    $(window).resize(function() {
+      fixHeight();
+    });
+  
+    $('.navbar .navbar-toggler').on('click', function() {
+      fixHeight();
+    });
+  
+  
+    $('.navbar-toggler,.overlay,.btnclsnav').on('click', function() {
+      $('.mobileMenu,.overlay,.btnclsnav').toggleClass('open');
+    });
+  
+  
+  
+  //for banner portion
+    $('.carousel').carousel({
+      interval: 3000,
+      keyboard: true,
+      pause: 'hover',
+      wrap: true
+    });
+  
+    $('#slider4').on('slide.bs.carousel', function () {
+      console.log('SLIDE!');
+    });
+  
+    $('#slider4').on('slid.bs.carousel', function () {
+      console.log('SLID!');
+    });
+  
+  
+  
+    //current year for copyright
+    document.getElementById("footeryear").innerHTML=new Date().getFullYear();
+  
+  
+  
+  
+  
+  });
